ba5aad367519843605cae2403223866766af9fc0
Mats Tornberg <mats@tornberg.me>
2022-02-18 09:56:47 +0100

    cleanup

--- dist/components/Partial.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const createelement_1 = require("../lib/createelement");
-const Partial = ({ items }) => {
-    const elms = items.map((item) => (0, createelement_1.h)("span", null, item));
-    return (0, createelement_1.h)("div", null, elms);
-};
-exports.default = Partial;
--- dist/lib/flatDeep-polyfill.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.flattenDeep = void 0;
-function flattenDeep(arr) {
-    // Polyfill for array.flat(Infinity).
-    return arr.reduce((acc, val) => (Array.isArray(val) ? acc.concat(flattenDeep(val)) : acc.concat(val)), []);
-}
-exports.flattenDeep = flattenDeep;
