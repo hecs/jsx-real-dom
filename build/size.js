04844ad5ed9d3d619a3ab4a5b0665650b5786e94
Henrik Friberg <henrik.friberg@gmail.com>
2022-04-28 00:00:42 +0200

    Better size message

--- build/size.js
+++ build/size.js
@@ -4,4 +4,4 @@ var fs = require("fs");
 var str = fs.readFileSync("temp/sizetest.js").toString();
 const compressedSize = brotliSize.sync(str);
 
-console.info("Minified & brotli-compressed bundle size:", compressedSize);
+console.info("Minified & brotli-compressed bundle size:", compressedSize, "bytes");
