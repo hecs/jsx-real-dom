24ace3f69798b5b5c0957d807c42c35ba78deafb
Mats Törnberg <mats.tornberg@gmail.com>
2022-08-26 07:24:09 +0200

    update lib target and observable

--- src/lib/utils/observable.ts
+++ src/lib/utils/observable.ts
@@ -1,11 +1,13 @@
-export type Transform<I, O> = (data: I) => I | O | Promise<O>;
+export type TransformCallback<O> = (result: O) => void;
+export type Transform<I, O> = (data: I, resolve: TransformCallback<O | I>) => void;
+export type ObservedValueOf<I, O> = O extends Observable<I, infer T> ? T : never;
 
 export class Observable<I, O> {
     _next: Observable<O, unknown>[] = [];
-    input: any;
-    output: any;
+    input: I | undefined;
+    output: O | undefined;
     transformer: Transform<I, O>;
-    constructor(data?: I | undefined, transformFunction: Transform<I, O> = (d) => d) {
+    constructor(data?: I | undefined, transformFunction: Transform<I, O> = (d, r) => r(d)) {
         this.transformer = transformFunction.bind(this);
         if (data !== undefined) {
             this.emit(data);
@@ -19,48 +21,48 @@ export class Observable<I, O> {
         return this;
     }
     handleOutput(data: any, force: boolean = false) {
-        if (force || data != this.output) {
+        if (force || (data != this.output && data !== undefined)) {
             this.output = data;
             this._next.forEach((next) => {
-                next.emit(this.output);
+                next.emit(this.output!);
             });
         }
     }
     #onChange() {
-        const result = this.transformer(this.input);
-        //console.log("got result", result);
-        if (result instanceof Promise) {
-            result.then((o) => {
-                this.handleOutput(o);
+        if (this.input !== undefined) {
+            this.transformer(this.input, (result) => {
+                this.handleOutput(result);
             });
-        } else {
-            this.handleOutput(result);
         }
     }
-    next<R extends Observable<O, any>>(n: R) {
+    next<R>(n: Observable<O, R>) {
         this._next.push(n);
         if (this.output !== undefined) {
             n.emit(this.output);
         }
         return n;
     }
-    pipe<R extends Observable<O, any>>(...next: R[]): R {
-        let t: any = this;
-        next.forEach((n) => {
-            t = t.next(n);
-        });
-        return t;
-    }
-    subscribe(fn: (data: O) => unknown) {
-        this.next(tap(fn));
+    // pipe<R>(f:Observable<O,R>, ...next: Observable<unknown, unknown>[]) {
+    //     this.next(f).pipe(...next);
+    //     let t: any = this;
+    //     next.forEach((n) => {
+    //         t = t.next(n);
+    //     });
+    //     return t;
+    // }
+    subscribe(fn: (data: O) => void) {
+        this.next(
+            new Observable<O, O>(undefined, (data, cb) => {
+                fn(data);
+                cb(data);
+            })
+        );
         return this;
     }
 }
 
 export const of = <I>(data: I | Promise<I>) => {
-    return data instanceof Promise<I>
-        ? new Observable<unknown, I>(1, () => data)
-        : new Observable<I, I>(data);
+    return data instanceof Promise<I> ? map(() => data) : new Observable<I, I>(data);
 };
 
 export const tap = <I>(fn: (data: I) => void) =>
@@ -69,7 +71,15 @@ export const tap = <I>(fn: (data: I) => void) =>
         return data;
     });
 
-export const map = <I, O>(fn: Transform<I, O>) => new Observable<I, O>(undefined, fn);
+export const map = <I, O>(fn: (data: I) => O | Promise<O>) =>
+    new Observable<I, O>(undefined, (data, cb) => {
+        const r = fn(data);
+        if (r instanceof Promise) {
+            r.then(cb);
+        } else {
+            cb(r);
+        }
+    });
 
 export const timer = <I extends any>(value: I, time: number = 300) => {
     const o = new Observable<I, I>();
@@ -79,66 +89,57 @@ export const timer = <I extends any>(value: I, time: number = 300) => {
     return o;
 };
 
-export const delay = (time: number = 200) =>
-    new Observable<any, any>(
-        undefined,
-        (d) =>
-            new Promise((res) => {
-                setTimeout(() => {
-                    res(d);
-                }, time);
-            })
-    );
+export const delay = <I>(time: number = 200) => {
+    let timer;
+    return new Observable<I, I>(undefined, (d, cb) => {
+        if (timer) {
+            clearTimeout(timer);
+        }
+        timer = setTimeout(() => {
+            cb(d);
+        }, time);
+    });
+};
 
 type ForkInput = {
     [key: string]: Observable<any, any>;
 };
 
+export const push = <A, B>(arg: Observable<any, B>) => {
+    return new Observable<A, [A, B]>(undefined, function (a, resolve) {
+        arg.subscribe((b) => {
+            resolve([a, b]);
+        });
+    });
+};
+
 export const forkJoin = <T extends ForkInput, O extends { [key in keyof T]: any }>(parts: T) => {
-    return new Observable<boolean, O>(true, function (_) {
-        const t = this;
+    return new Observable<boolean, O>(true, function (_, resolve) {
+        const checkComplete = () => {
+            if (!Object.values(o).some((d) => d === undefined)) {
+                resolve({ ...o } as O);
+            }
+        };
 
-        return new Promise<O>((_) => {
-            const checkComplete = () => {
-                if (!Object.values(o).some((d) => d === undefined)) {
-                    t.handleOutput({ ...o }, false);
-                }
-            };
+        const o = Object.entries(parts).reduce((sum, [key]) => {
+            return { ...sum, [key]: undefined };
+        }, {});
 
-            const o = Object.entries(parts).reduce((sum, [key]) => {
-                return { ...sum, [key]: undefined };
-            }, {});
-
-            Object.entries(parts).forEach(([key, value]) => {
-                value.subscribe((v) => {
-                    o[key] = v;
-                    checkComplete();
-                });
+        Object.entries(parts).forEach(([key, value]) => {
+            value.subscribe((v) => {
+                o[key] = v;
+                checkComplete();
             });
         });
     });
 };
 
-export const fromEvent = <
-    T extends
-        | Event
-        | ClipboardEvent
-        | UIEvent
-        | AnimationEvent
-        | MouseEvent
-        | InputEvent
-        | FocusEvent
-        | any,
-    E extends HTMLElement | Window,
-    O
->(
-    elm: E,
-    evt: keyof HTMLElementEventMap | keyof WindowEventMap = "click",
-    initialValue?: T,
-    transform?: Transform<T, O>
+export const fromEvent = <T extends Event>(
+    elm: HTMLElement | Window,
+    evt: keyof HTMLElementEventMap | keyof WindowEventMap = "click"
 ) => {
-    const obs = new Observable<T, O>(initialValue, transform);
-    elm.addEventListener(evt, (e: Event) => {
+    const obs = new Observable<T, T>();
+    elm.addEventListener(evt, (e) => {
         obs.emit(e as T);
     });
     return obs;
